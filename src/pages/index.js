619a3f2ba3f00e040f36331d4e69bc42255c30b2
digitrans dev <developers@digi-trans.org>
2025-01-13 21:20:50 +0100

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Welcome to My Site</h1>
+    <p>This is a refactored version of the original site.</p>
+  </Layout>
+)
+
+export default IndexPage 
